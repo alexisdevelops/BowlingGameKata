03f915924abfe9ffe4c76386b4fb7fd7fd17ff61
alexisdevelops <alexisdevelops@gmail.com>
2020-11-25 21:06:03 -0800

    Saving project to repo

--- BowlingGameTests/BowlingGameTests.swift
+++ BowlingGameTests/BowlingGameTests.swift
@@ -9,25 +9,52 @@ import XCTest
 @testable import BowlingGame
 
 class BowlingGameTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    private var game: Game!
+    
+    override func setUp() {
+        super.setUp()
+        game = Game()
+    }
+    
+    override func tearDown() {
+        game = nil
+        super.tearDown()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testGutterGame(){
+        rollMany(pins: 0, times: 20)
+        XCTAssertEqual(game.score(), 0)
     }
+    
+    func testaGameWithAllOnes() {
+        rollMany(pins: 1, times: 20)
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(game.score(), 20)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testOneSpare() {
+        game.roll(0)
+        game.roll(10)
+        game.roll(3)
+        XCTAssertEqual(game.score(), 16)
+    }
+    
+    func testOneStrike() {
+        game.roll(10)
+        game.roll(3)
+        game.roll(4)
+        XCTAssertEqual(game.score(), 24)
+    }
+    
+    func testPerfectGame() {
+        rollMany(pins: 10, times: 12)
+        XCTAssertEqual(game.score(), 300)
+    }
+    
+    private func rollMany(pins: Int, times: Int) {
+        for _ in 1...times {
+            game.roll(pins)
         }
     }
-
 }
